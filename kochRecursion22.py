3e8b06f0f5c15a99fe9bb30fb217f89dbe468f7c
Greg K <gkreisman_cep@qq.com>
2022-06-29 09:12:48 +0800

    add snowflake

--- kochRecursion22.py
+++ kochRecursion22.py
@@ -6,10 +6,12 @@ wn.screensize(800,800)
 
 greg = turtle.Turtle()
 wn.bgcolor((0,0,0))
+wn.tracer(False) ## turns animation off
+greg.hideturtle() ## need to do this for tracer animation off
 greg.speed(0)
 greg.color("green")
 greg.penup()
-greg.goto(-400,0)
+greg.goto(-200,100)
 greg.pendown()
 
 
@@ -29,5 +31,15 @@ def koch(t, order, size):
 
 
 
-koch(greg,3,600)
-turtle.exitonclick()
+def snowflake(t,order,size):
+    koch(t,order,size)
+    t.right(120)
+    koch(t,order,size)
+    t.right(120)
+    koch(t,order,size)
+
+#koch(greg,3,600)
+
+snowflake(greg,2,400)
+
+wn.exitonclick()
